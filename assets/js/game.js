8a5d4516e77037c6bdd6b0da09cdb596bfa77432
Marcos Minond <minond@beeblebrox.(none)>
2013-02-03 21:40:11 -0700

    changes to meet assigment requirements

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 var Boards, Board, myboard;
 
 Board = new Model({
@@ -13,3 +14,26 @@ myboard = Boards.create({
 	rows: 20,
 	columns: 20
 });
+*/
+
+Template.config.load.auto = false;
+
+var board_holder, generate_game_board = function(width, height, type) {
+	var tablestr = [ "<table class='board ", type, "'>" ];
+
+	for (var i = 0; i < width; i++) {
+		tablestr.push("<tr>");
+		for (var j = 0; j < height; j++) {
+			tablestr.push("<td></td>");
+		}
+		tablestr.push("</tr>");
+	}
+
+	tablestr.push("</table>");
+	return tablestr.join("");
+};
+
+window.onload = function() {
+	board_holder = document.getElementById("board_holder");
+	board_holder.innerHTML = generate_game_board(10, 10, "player");
+};

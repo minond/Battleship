2036e284642e28c5e35ab95b3da0b003da886694
Marcos Minond <minond.marcos@gmail.com>
2013-03-04 22:02:05 -0700

    backup - starting assigment 4

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,39 +1,21 @@
 "use strict";
 
-var Tabular = Polypus.Service.api.get_service_from_cache("Tabular");
-var Persist = Polypus.Service.api.get_service_from_cache("Persist");
-var Sync = Polypus.Service.api.get_service_from_cache("Sync");
-
-var theirboard = battleship.Boards.create({
-	title: "Your Opponent",
-	rows: 20,
-	columns: 20,
-	owner: battleship.Board.owner.opponent
-});
-
-var myboard = battleship.Boards.create({
+battleship.myboard = battleship.Boards.create({
 	title: "Player 1",
 	rows: 20,
 	columns: 20,
 	owner: battleship.Board.owner.player
 });
 
-battleship.Battleship = new Polypus.Controller({
-	selected: null,
-	select: function(ship, $BattleshipGame) {
-		console.log(arguments);
-		this.selected = ship;
-	}
-}, {
-	// select a ship to be added to the board
-	"click #ships .ship": function(click, el) {
-		var ship = battleship.Ships.get_by_id(el.dataset.shipId);
-		this.select(ship);
-		console.log(click, el, ship);
-	},
-
-	// hover a ship on the board
+battleship.Battleship = new Polypus.Controller({}, {
 	"mouseover table.board.player tr td": function(mouse, el) {
 		console.log(mouse, el);
 	}
+});
+
+Polypus.eventuum.load(function() {
+	// battleship.Modal.show(
+		// Polypus.Template.tmpl.player_name.render(battleship.myboard), "Battleship", "auto");
+	// Polypus.adjutor.dataset(
+		// document.getElementById("player_name"), "model", battleship.myboard);
 });
--- assets/js/models/ships.js
+++ assets/js/models/ships.js
@@ -6,16 +6,16 @@ battleship.Ships.create({
 	src: "assets/img/clipper.jpg",
 	length: 5,
 	width: 2,
-	description: "Enim vel in, nascetur tincidunt ridiculus, rhoncus odio magnis habitasse vel placerat ut cursus! Rhoncus eu magna. Vel turpis dignissim. In parturient non proin! Hac nisi sed sed, facilisis auctor, etiam amet egestas. Dictumst? Elit porttitor! Cras in et, turpis, porta pulvinar, proin nec? Phasellus habitasse! Nec lacus, placerat urna et cum nisi nec?"
+	description: "A clipper was a very fast sailing ship of the 19th century that had three or more masts and a square rig. They were generally narrow for their length, could carry limited bulk freight, small by later 19th century standards, and had a large total sail area. Clipper ships were mostly made in British and American shipyards, though France, the Netherlands and other nations also produced some. Clippers sailed all over the world, primarily on the trade routes between the United Kingdom and its colonies in the east, in trans-Atlantic trade, and the New York-to-San Francisco route round Cape Horn during the California Gold Rush. Dutch clippers were built beginning in 1850s for the tea trade and passenger service to Java."
 });
 
-// Friate
+// Frigate
 battleship.Ships.create({
-	name: "Dutch Friate",
-	src: "assets/img/dutch_friate.jpg",
+	name: "Dutch Frigate",
+	src: "assets/img/dutch_frigate.jpg",
 	length: 4,
 	width: 2,
-	description: "Urna enim arcu, nisi pellentesque amet nunc nascetur. Turpis amet lundium tortor hac ultricies placerat, arcu elit? Diam ultricies sed enim hac pid? Cursus phasellus et, sed, pid natoque mid tincidunt ut rhoncus in sit tincidunt mus, magnis sed, pulvinar et cursus vel risus augue mauris mid nec nec, natoque habitasse, cursus. Magna adipiscing pulvinar, ac magnis dictumst mid. In porta sagittis egestas etiam platea non montes, rhoncus? Sociis augue lacus, elementum natoque nisi."
+	description: "A frigate is any of several types of warship, the term having been used for ships of various sizes and roles over the last few centuries. In the 17th century, the term was used for any warship built for speed and maneuverability, the description often used being \"frigate-built\". These could be warships carrying their principal battery of carriage-mounted guns on a single deck or on two decks (with further smaller carriage-mounted guns usually carried on the forecastle and quarterdeck of the vessel). The term was generally used for ships too small to stand in the line of battle, although early line-of-battle ships were frequently referred to as frigates when they were built for speed."
 });
 
 // Pirate Ship
@@ -24,7 +24,7 @@ battleship.Ships.create({
 	src: "assets/img/pirate_ship.gif",
 	length: 2,
 	width: 1,
-	description: "Velit ac ac ultrices aenean vel, tempor phasellus turpis, mus dignissim, porttitor sit urna! Nec sed vel, lacus? Cursus. Turpis, quis massa! Aenean tincidunt! Placerat nunc amet est!"
+	description: "It may be reasonable to assume that piracy has existed for as long as the oceans were plied for commerce. The earliest documented instances of piracy are the exploits of the Sea Peoples who threatened the Aegean and Mediterranean in the 14th century BC."
 });
 
 // The Nina
@@ -33,7 +33,7 @@ battleship.Ships.create({
 	src: "assets/img/the_nina.jpg",
 	length: 3,
 	width: 1,
-	description: "Odio habitasse adipiscing odio ridiculus ridiculus proin dictumst natoque! Habitasse ut sagittis aliquet, natoque nec cum cras dapibus urna placerat? Sagittis mus quis! Ac, mauris nunc? Massa? In? Nec mus, purus sagittis tortor duis a in, et cum tincidunt vut parturient aliquam sit ac purus augue turpis pulvinar nunc tristique placerat ultricies nisi dapibus vel magna? Sagittis. Augue et nisi, non sociis et placerat turpis nunc? Turpis dictumst et vut lundium rhoncus ac turpis sit habitasse sit velit!"
+	description: "La Niña (Spanish for The Girl) was one of the three ships used by Christopher Columbus in his first voyage towards the Indies in 1492. The ship was originally called the Santa Clara, but was changed to La Niña. The name Niña was probably a pun on the name of her owner, Juan Niño of Moguer. She was a standard caravel-type vessel."
 });
 
 // The Pinta
@@ -42,7 +42,7 @@ battleship.Ships.create({
 	src: "assets/img/the_pinta.jpg",
 	length: 3,
 	width: 1,
-	description: "Dolor penatibus porttitor, adipiscing proin lundium purus! Natoque sit, porttitor adipiscing, in nec mid in in duis! Elementum sed? Et ut ultricies cras ac scelerisque in aliquet phasellus sit! Integer nunc in eu arcu et? Pulvinar. Auctor, pellentesque! Habitasse pulvinar ac enim facilisis. Vel ac turpis, dis auctor amet aliquet egestas elit enim velit rhoncus? Aliquam cras. Nec dignissim vel est! Porta phasellus pulvinar habitasse porttitor tincidunt tempor aliquam, duis augue elit nisi. Magnis elementum nunc! Nunc nec placerat, augue scelerisque magnis aliquet etiam, ac. Cursus nisi, hac placerat sed, lorem phasellus sed dis? Dignissim? Etiam elementum, lacus elit ultricies placerat ut? Adipiscing facilisis integer. Lectus sed ridiculus augue integer, lacus tristique amet augue proin."
+	description: "La Pinta (Spanish for The Painted One or The Spotted One) was the fastest of the three ships used by Christopher Columbus in his first transatlantic voyage in 1492. The New World was first sighted by Rodrigo de Triana on the Pinta on October 12, 1492. The owner of the Pinta was Cristobal Quintero. The Quintero brothers were ship owners from Palos. The owner of the ship allowed Captain Martin Alonso Pinzon to take over the ship so he can keep an eye on the ship."
 });
 
 // The Santa Maria
@@ -51,5 +51,5 @@ battleship.Ships.create({
 	src: "assets/img/the_santa_maria.jpg",
 	length: 3,
 	width: 1,
-	description: "Tincidunt et porttitor habitasse turpis elementum ridiculus purus porta vel sociis nunc pulvinar cursus odio massa ultricies? Magna non amet ac. Proin turpis mauris. Nec sed eu est, aliquam. In? In lorem, augue amet placerat augue, porttitor vel! Pid adipiscing, nascetur turpis dignissim ac tincidunt? Vut porttitor nisi! Aenean porttitor nisi cursus et lacus? Nec lectus! Ultrices natoque turpis tincidunt, quis aliquet elit purus? Tortor nisi nec magna ut pellentesque habitasse, tristique? Purus penatibus magnis in? Sed amet sed placerat, magna augue risus massa? Scelerisque. Adipiscing nisi, habitasse, pellentesque tincidunt ac arcu! In est nisi vel, porta ac sed, in platea odio integer dolor pulvinar in porta montes pellentesque habitasse augue in nisi tortor platea duis. Pellentesque. Adipiscing? Et egestas."
+	description: "The Santa María was probably a medium sized nao (carrack), about 58 ft (17.7 m) long on deck, \"very little larger than 100 toneladas\" (100 tons, or tuns) burthen, or burden, and was used as the flagship for the expedition."
 });
